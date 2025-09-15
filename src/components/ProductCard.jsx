2fcccb49f516ce95348680be080155d8423820e5
Wilfox0 <wilfoxlove@gmail.com>
2025-09-16 01:25:36 +0300

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useCart } from '../CartContext';
 
 const ProductCard = ({ product, addToCart }) => {
   const [selectedSize, setSelectedSize] = useState(product.sizes[0] || '');
