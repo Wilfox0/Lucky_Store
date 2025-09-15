dc91192837972525543187c2f543167a3b59878b
Wilfox0 <wilfoxlove@gmail.com>
2025-09-16 01:14:13 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// إعدادات Firebase الخاصة بالمشروع
 const firebaseConfig = {
   apiKey: "AIzaSyDFEQYcYRC_A_Lpe4iSlzbe9NNEwxqpBQI",
   authDomain: "lacky-store.firebaseapp.com",
@@ -11,7 +12,9 @@ const firebaseConfig = {
   appId: "1:715637340163:web:5c54c5494bddec1f9c3636"
 };
 
+// تهيئة التطبيق
 const app = initializeApp(firebaseConfig);
 
+// التصدير لاستخدامه في باقي الملفات
 export const db = getFirestore(app);
 export const storage = getStorage(app);
