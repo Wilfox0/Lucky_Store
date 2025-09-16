f6d5a35cbbe1d5fc1b0dabcaa3968ac257ecac50
Wilfox0 <wilfoxlove@gmail.com>
2025-09-16 18:00:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 // src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+root.render(<App />);
