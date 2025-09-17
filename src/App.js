fe804b825f5fa0ff9e343bc618132dfaf5adc15b
Wilfox0 <wilfoxlove@gmail.com>
2025-09-17 03:42:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { db } from "../firebase";
+import { db } from "./firebase";
 import { doc, getDoc } from "firebase/firestore";
 
 const ProductDetail = ({ addToCart }) => {
