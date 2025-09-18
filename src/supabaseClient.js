dc2af4c1f2093307d4f86ae6dc775a2277ee70a0
Wilfox0 <wilfoxlove@gmail.com>
2025-09-18 16:01:29 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,1 +1,12 @@
+// src/supabaseClient.js
+import { createClient } from "@supabase/supabase-js";
+
+// رابط مشروعك على Supabase
+const SUPABASE_URL = "https://hetysfxvbbgunqxmcauu.supabase.co";
+
+// المفتاح الذي حصلت عليه (API Key)
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhldHlzZnh2YmJndW5xeG1jYXV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxMjI4MTMsImV4cCI6MjA3MzY5ODgxM30.o8v4wXJOzDkxsrhQQnVFlsaDAwJrdmlvpPwhD74HGdg";
+
+// إنشاء عميل Supabase
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
